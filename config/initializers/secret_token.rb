a79151ed54a6585e42fc254dcc4ca5b5f3692de8
Tom Powell <tompowell@gmail.com>
2012-02-19 18:35:47 -0500

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Herokio::Application.config.secret_token = '3b2a7b96aae0c5efc02f7c140a01a8d88882306047c3d9ca536ff3c5dd21c284b0c1698e11a1e3033877dedfb0c3c2ec7d61c130b2740eddd6e7b838f19b9d25'
